6acc807db1ced10cc9d78aa7a4158add0fea61d6
JavaGirl18 <valenciacooper89@gmail.com>
2018-07-27 23:24:08 -0400

    links to one event in show user works

--- client/src/components/ShowUser.js
+++ client/src/components/ShowUser.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import { Button, Card, Image } from 'semantic-ui-react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
+const Events = styled.div`
+display: flex;
+flex-wrap:wrap;
+
+
+`
 class ShowUser extends Component {
 state={
     user: {},
@@ -48,7 +57,7 @@ getUser = (userId) => {
             const eventId = event.id
             // const eachEvent = `/events/${eventId}/organizers/${organizerId}`
             return (
-                <div>
+                <div key={index}>
 
                     <ul>
                         {/* <Link to={eachOrganizer}> {organizer.name} </Link> */}
@@ -63,7 +72,7 @@ getUser = (userId) => {
             // const eventId = event.id
             // const eachEvent = `/events/${eventId}/organizers/${organizerId}`
             return (
-                <div>
+                <div key={index}>
 
                     <ul>
                         {/* <Link to={eachOrganizer}> {organizer.name} </Link> */}
@@ -74,30 +83,50 @@ getUser = (userId) => {
         })
 
         const attendeeEvents = this.state.attendeeEvents.map((attenEvent, index) => {
-            // const eventId = event.id
-            // const eachEvent = `/events/${eventId}/organizers/${organizerId}`
+            const eventId = attenEvent.id
+            const eachEvent = `/events/${eventId}`
             return (
-                <div>
+                <div key={index}>
 
                     <ul>
                         {/* <Link to={eachOrganizer}> {organizer.name} </Link> */}
-                        {attenEvent.title}
+                        <Card.Group>
+            <Card>
+                        <Card.Content>
+                        <Image floated='right' size='mini' src='https://react.semantic-ui.com/images/avatar/large/steve.jpg' />
+                        <Link to={eachEvent}> <Card.Header>{attenEvent.title}</Card.Header></Link>
+                <Card.Meta>{attenEvent.date}</Card.Meta>
+                {/* <Card.Description>
+                  Steve wants to add you to the group <strong>best friends</strong>
+                </Card.Description> */}
+                <Card.Content extra>
+        <div className='ui two buttons'>
+         <Link to=''><Button basic color='green'>View</Button></Link>
+         <Link to=''><Button basic color='red'>Remove </Button></Link>
+        </div>
+      </Card.Content>
+                </Card.Content>
+                </Card>
+              </Card.Group>
                     </ul>
                     </div>
             )
         })
 
         return (
-
-
+<div>
+           
             <div>
              <h4> {this.state.user.name}</h4>
                 <p> {this.state.user.email}</p> 
                <p>{this.state.user.number}</p> 
-                <h1>Events you organized</h1>
+                <h1>Events You've Organized</h1>
                {organizerEvents}
-               <h1>Events You've attended</h1>
+               <h1>Events You've Attended</h1>
+               <Events>
                {attendeeEvents}
+               </Events>
+            </div>
             </div>
         );
     }
